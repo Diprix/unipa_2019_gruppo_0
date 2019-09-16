071edf545c42d2c8cdf45f62d8a39611891888dd
davideag81 <davide_ag@hotmail.it>
2019-09-16 22:20:48 +0200

    Integrazione WebPushService

--- filesharing-backend/src/main/java/it/eng/unipa/filesharing/container/SendController.java
+++ filesharing-backend/src/main/java/it/eng/unipa/filesharing/container/SendController.java
@@ -1,14 +1,24 @@
 package it.eng.unipa.filesharing.container;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.eng.unipa.filesharing.model.WebPushMessage;
+import it.eng.unipa.filesharing.model.WebPushSubscription;
+import org.jose4j.lang.JoseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import com.google.gson.Gson;
 import org.apache.http.HttpResponse;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import nl.martijndwars.webpush.Subscription;
 import nl.martijndwars.webpush.PushService;
 import nl.martijndwars.webpush.Notification;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.Security;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 @RestController
@@ -17,6 +27,41 @@ public class SendController {
     private static final String PRIVATE_KEY = "A7xDGuxMZ4ufflcAhBW23xpoWZNOLwM4Rw2wXjP0y6M";
     private static final String SUBJECT = "Foobarbaz";
     private static final String PAYLOAD = "My fancy message";
+    private static PushService pushService = new PushService();
+
+    private Map<String, WebPushSubscription> subscriptions = new ConcurrentHashMap<>();
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @PostMapping("/subscribe")
+    public void subscribe(WebPushSubscription subscription) {
+
+        subscriptions.put(subscription.getNotificationEndPoint(), subscription);
+    }
+
+    @PostMapping("/unsubscribe")
+    public void unsubscribe(WebPushSubscription subscription) {
+
+        subscriptions.remove(subscription.getNotificationEndPoint());
+    }
+
+    @PostMapping("/notify-all")
+    public WebPushMessage notifyAll(@RequestBody WebPushMessage message) throws GeneralSecurityException, IOException, JoseException, ExecutionException, InterruptedException {
+
+        for (WebPushSubscription subscription: subscriptions.values()) {
+
+            Notification notification = new Notification(
+                    subscription.getNotificationEndPoint(),
+                    subscription.getPublicKey(),
+                    subscription.getAuth(),
+                    objectMapper.writeValueAsBytes(message));
+
+            pushService.send(notification);
+        }
+
+        return message;
+    }
 
     @RequestMapping("/send")
     public String send(@RequestParam("subscriptionJson") String subscriptionJson) {
---
+++ filesharing-backend/src/main/java/it/eng/unipa/filesharing/model/WebPushMessage.java
@@ -0,0 +1,8 @@
+package it.eng.unipa.filesharing.model;
+
+public class WebPushMessage {
+
+    public String title;
+    public String clickTarget;
+    public String message;
+}
