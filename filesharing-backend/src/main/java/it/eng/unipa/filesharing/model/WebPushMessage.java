6265675e08e4809c1faab65bb272e2c5da9082b4
davideag81 <davide_ag@hotmail.it>
2019-09-17 09:20:39 +0200

    Integrazione WebPushService deselezionate JPA webPush

--- filesharing-backend/src/main/java/it/eng/unipa/filesharing/model/WebPushMessage.java
+++ filesharing-backend/src/main/java/it/eng/unipa/filesharing/model/WebPushMessage.java
@@ -1,13 +1,22 @@
 package it.eng.unipa.filesharing.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
-
+//@Entity
 public class WebPushMessage {
-
+   // @Id
     private final UUID uuid;
-    private final String description;
+
+    private  String description= null;
     private  Object endPoint = null;
 
+    //IMPLEMENT FOREIN KEY CONSTRACT
+
+    public WebPushMessage(UUID uuid) {
+        // TODO Auto-generated constructor stub
+        this.uuid = UUID.randomUUID();
+    }
 
     public WebPushMessage(UUID creator, String description, Object endPoint) {
         this.uuid = UUID.randomUUID();
